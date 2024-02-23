e9ded61cf4ef640e926e0feced710c88437e00fe
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-02-23 12:54:01 +0100

    fix ads banner

--- components/ads/adsBanner.component.tsx
+++ components/ads/adsBanner.component.tsx
@@ -4,19 +4,17 @@ import { isReleased } from "@/src/utils/envs.utils";
 
 const AdsBanner = () => {
   return (
-    <div className="relative w-full">
-      <div className="mx-auto max-w-screen-xl inline-flex justify-center py-4 px-4 sm:px-6 lg:px-8">
-        {isReleased ? (
-          <iframe
-            style={{ backgroundColor: "white" }}
-            width="900"
-            height="250"
-            allowTransparency={true}
-            name="spot_id_10006954"
-            src="//a.adtng.com/get/10006954?ata=gheart"
-          ></iframe>
-        ) : null}
-      </div>
+    <div className="relative w-full mx-auto inline-flex justify-center py-4 px-4 sm:px-6 lg:px-8">
+      {isReleased ? (
+        <iframe
+          style={{ backgroundColor: "white" }}
+          width="900"
+          height="250"
+          allowTransparency={true}
+          name="spot_id_10006954"
+          src="//a.adtng.com/get/10006954?ata=gheart"
+        ></iframe>
+      ) : null}
     </div>
   );
 };
