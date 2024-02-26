d2b57bd167a2d98f78927ca3f3af531e5ed9082b
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-02-26 10:34:57 +0100

    init analytics events logging

--- app/content-redirect/[contentUrl]/contentRedirect.component.tsx
+++ app/content-redirect/[contentUrl]/contentRedirect.component.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { ButtonLink } from "@/components";
 import AdsBlock from "@/components/ads/adsBlock.component";
 import { AdsBlockTypeEnum } from "@/src/enums/ads.enums";
+import { AnalyticsEventEnum } from "@/src/enums/analytics.enums";
+import { AnalyticsUtils } from "@/src/utils/analytics.utils";
 import { useEffect, useState } from "react";
 
 type ContentRedirectComponentI = {
@@ -20,13 +23,20 @@ const ContentRedirectComponent = (props: ContentRedirectComponentI) => {
     <>
       <div className="w-full inline-flex items-center justify-center sm:justify-end">
         {contentUrl ? (
-          <ButtonLink
-            isLoading={continueDisabled}
-            disabled={continueDisabled}
-            primary
-            label="Continue to video"
-            href={decodeURIComponent(contentUrl)}
-          />
+          <span
+            onClick={() => {
+              if (!continueDisabled)
+                AnalyticsUtils.logEvent(AnalyticsEventEnum.GO_TO_VIDEO);
+            }}
+          >
+            <ButtonLink
+              isLoading={continueDisabled}
+              disabled={continueDisabled}
+              primary
+              label="Continue to video"
+              href={decodeURIComponent(contentUrl)}
+            />
+          </span>
         ) : null}
       </div>
       <div className="mx-auto mt-8">
