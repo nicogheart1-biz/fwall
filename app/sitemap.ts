8f31ca57320f09596c5ea6efff859e4ccc3db617
Niccolo Valente <36986000+nicogheart1@users.noreply.github.com>
2024-02-27 16:19:33 +0100

    merge develop (#4)
    * fix ads banner
    
    * fix ads banner
    
    * various fix
    
    * video card improvements
    
    * improvements
    
    * fix
    
    * rm playground
    
    * init redtube
    
    * debug
    
    * debug
    
    * init active providers
    
    * add rating meta tag
    
    * init analytics events logging
    
    * add ig url
    
    * fix videocard component
    
    * init pagination component
    
    * wall improvements
    
    * fix videoCardPreview on mobile
    
    * init RTA
    
    * minor changes
    
    * init trending videos
    
    * init tags page
    
    * fix image
    
    * fix menu mobile
    
    * fix for SEO
    
    * video card improvement
    
    * fix style
    
    * init vertical ads banner
    
    * update page queries
    
    * update wall ads
    
    * fix video card
    
    * seo improvements
    
    ---------
    
    Co-authored-by: Niccolò Valente <niccolo.valente.biz@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,24 +1,45 @@
 import { MetadataRoute } from "next";
 import Routes from "@/src/assets/routes-en.json";
 import { AppConstants } from "@/src/constants";
+import TagList from "@/mock/tags/tags.json";
 
-const forcedRoutes: MetadataRoute.Sitemap = [
-  {
-    url: `${AppConstants.hostUrl}${Routes.home.url}`,
-    lastModified: new Date(),
-    changeFrequency: "hourly",
-    priority: 1,
-  },
-];
+const forcedRoutes: MetadataRoute.Sitemap = [];
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const routes: MetadataRoute.Sitemap = Object.values(Routes).map((route) => ({
-    url: `${AppConstants.hostUrl}${route.url}`,
-    lastModified: new Date(),
-    changeFrequency: "monthly",
-    priority: 0.8,
-  }));
-  return [...forcedRoutes, ...routes].filter(
+  const routes: MetadataRoute.Sitemap = Object.values(Routes)
+    .filter((route) => {
+      // @ts-ignore
+      if (route?.seo?.index === false) {
+        return;
+      } else {
+        return route;
+      }
+    })
+    .map((route) => ({
+      url: `${AppConstants.hostUrl}${route.url}`,
+      lastModified: new Date(),
+      // @ts-ignore
+      changeFrequency: route.seo?.changeFrequency || "monthly",
+      // @ts-ignore
+      priority: route.seo?.priority || 0.8,
+    }));
+
+  const calculatedRoutes: MetadataRoute.Sitemap = [];
+
+  TagList.tags.forEach((tag) => {
+    calculatedRoutes.push({
+      url: `${AppConstants.hostUrl}${Routes.tags.url}/${tag
+        .toLowerCase()
+        .replaceAll(" ", "-")}`,
+      lastModified: new Date(),
+      // @ts-ignore
+      changeFrequency: "hourly",
+      // @ts-ignore
+      priority: 1,
+    });
+  });
+
+  return [...forcedRoutes, ...routes, ...calculatedRoutes].filter(
     (value, index, self) => index === self.findIndex((t) => t.url === value.url)
   );
 }
