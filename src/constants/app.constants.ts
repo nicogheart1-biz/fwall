e0bada3b164e82270b5567612f9eefdaa1dccb0a
Niccolò Valente <niccolo.valente.biz@gmail.com>
2024-03-04 09:59:46 +0100

    fix seo

--- src/constants/app.constants.ts
+++ src/constants/app.constants.ts
@@ -1,6 +1,6 @@
 export const AppConstants = {
   title: "Feet-Video.com",
   description:
-    "The ultimate sensory experience with our premier collection of fetish videos dedicated to the allure of feet. Immerse yourself in a realm where passion meets obsession as our curated selection of videos caters to every desire and fantasy. Explore a diverse array of content, ranging from sensual massages to playful teasing and everything in between. Whether you're a seasoned enthusiast or a curious newcomer, our website offers a safe and welcoming environment to explore your desires.",
+    "Discover our premier collection of feet fetish videos. Immerse yourself in a realm where passion meets obsession as our curated selection of videos caters to every desire and fantasy. Explore a diverse array of content, ranging from sensual massages to playful teasing and everything in between. Whether you're a seasoned enthusiast or a curious newcomer, our website offers a safe and welcoming environment to explore your desires.",
   hostUrl: "https://www.feet-video.com",
 };
